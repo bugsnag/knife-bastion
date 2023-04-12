84d778a1598b4d1a7c3a9587e0c7a85c593f3862
xiaowei liu <xiaowei.liu@smartbear.com>
2023-04-11 23:47:13 -0400

    add debug msg
    test
    
    test
    
    test
    
    test
    
    test
    
    test
    
    test
    
    test
    
    test
    
    test
    
    test
    
    test

--- lib/knife-bastion/berkshelf_socks_proxy.rb
+++ lib/knife-bastion/berkshelf_socks_proxy.rb
@@ -5,20 +5,52 @@ require_relative 'base_socks_proxy'
 Berkshelf.module_eval do
   # class ChefConnectionError < StandardError; end
   # class BerkshelfError < StandardError; end
+  # ridley_compat = RidleyCompat
+
   class << self
     alias_method :ridley_connection_without_bastion, :ridley_connection
 
     def ridley_connection(options = {}, &block)
-      proxy_host = '127.0.0.1'
-      proxy_port = ::Chef::Config[:knife][:bastion_local_port]
-      proxy = Net::HTTP::SOCKSProxy(proxy_host, proxy_port)
+        options[:proxy_host] = '127.0.0.1'
+        options[:proxy_port] = ::Chef::Config[:knife][:bastion_local_port]
+        puts "ridley_connection called #{options.inspect}"
 
-      ridley_connection_without_bastion(options) do |builder|
-        builder.options[:proxy] = {
-          host: proxy_host,
-          port: proxy_port
-        }
-      end
+        ridley_connection_without_bastion(options, &block)
     end
   end
 end
+
+Berkshelf::RidleyCompatAPI.module_eval do
+  alias_method :initialize_original, :initialize
+
+  def initialize(**opts)
+    proxy_host = opts.delete(:proxy_host)
+    proxy_port = opts.delete(:proxy_port)
+    puts "Initializing RidleyCompatAPI with options: #{opts.inspect}"
+    initialize_original(**opts)
+
+    if proxy_host && proxy_port
+      self.http_client = Chef::HTTP.new(self, builder: Chef::HTTP::Builder.new(self, middleware))
+      self.http_client.builder.adapter = Net::HTTP.SOCKSProxy(proxy_host, proxy_port)
+      puts "Added the http_client: #{http_client.inspect}"
+    end
+  end
+end
+
+Berkshelf::RidleyCompatAPI::ClassMethods.module_eval do
+  def new_client(**opts)
+    client = new(**opts)
+    puts "Created a new client: #{client.inspect}"
+    if block_given?
+      puts "Executing block with the client..."
+      yield client
+      puts "Block execution finished."
+    else
+      puts "No block provided."
+    end
+    # ensure
+    # FIXME: does Chef::HTTP support close anywhere?  this will just leak open fds
+  end
+end
+
+
