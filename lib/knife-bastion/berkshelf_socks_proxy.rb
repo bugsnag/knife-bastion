d0d070d81113ac167359cca4a5f46dc5145c9dc1
xiaowei liu <xiaowei.liu@smartbear.com>
2023-07-07 15:36:48 -0400

    try monkey patch RidleyCompatAPI again

--- lib/knife-bastion/berkshelf_socks_proxy.rb
+++ lib/knife-bastion/berkshelf_socks_proxy.rb
@@ -69,15 +69,15 @@ Chef::HTTP.module_eval do
       @http_client.open_timeout = @config[:open_timeout] if @config[:open_timeout]
       @http_client.read_timeout = @config[:read_timeout] if @config[:read_timeout]
       @http_client.start
+      ::Kernel.puts "~~~~http_client called~~~~ proxy added ~~~~"
       @http_client
     end
 
     def request(method, url, headers = {}, data = false)
-
+      ::Kernel.puts "~~~~HTTP request dump~~~~"
       puts "Chef::HTTP Request (#{method}): #{url}"
       puts "Headers: #{headers.inspect}"
       puts "Data: #{data.inspect}" if data
-
       # Call the original request method
       super(method, url, headers, data)
     end
