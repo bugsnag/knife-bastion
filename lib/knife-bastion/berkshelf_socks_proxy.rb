745124bfb9712ed7ea4875ab7c837a10ef2dc1db
xiaowei liu <xiaowei.liu@smartbear.com>
2023-07-07 16:20:36 -0400

    tweak and test

--- lib/knife-bastion/berkshelf_socks_proxy.rb
+++ lib/knife-bastion/berkshelf_socks_proxy.rb
@@ -2,22 +2,22 @@ require_relative 'base_socks_proxy'
 
 # Override `ridley_connection` method in `Berkshelf` to enable Socks proxy
 # for the connection.
-Berkshelf.module_eval do
-  class << self
-    alias_method :ridley_connection_without_bastion, :ridley_connection
+# Berkshelf.module_eval do
+#   class << self
+#     alias_method :ridley_connection_without_bastion, :ridley_connection
 
-    def ridley_connection(options = {}, &block)
-        options[:proxy_host] = '127.0.0.1'
-        options[:proxy_port] = ::Chef::Config[:knife][:bastion_local_port]
-        puts "ridley_connection called #{options.inspect}"
-        options[:proxy_host] = '127.0.0.1'
-        options[:proxy_port] = ::Chef::Config[:knife][:bastion_local_port]
-        puts "ridley_connection called #{options.inspect}"
+#     def ridley_connection(options = {}, &block)
+#         options[:proxy_host] = '127.0.0.1'
+#         options[:proxy_port] = ::Chef::Config[:knife][:bastion_local_port]
+#         puts "ridley_connection called #{options.inspect}"
+#         options[:proxy_host] = '127.0.0.1'
+#         options[:proxy_port] = ::Chef::Config[:knife][:bastion_local_port]
+#         puts "ridley_connection called #{options.inspect}"
 
-        ridley_connection_without_bastion(options, &block)
-    end
-  end
-end
+#         ridley_connection_without_bastion(options, &block)
+#     end
+#   end
+# end
 
 # Berkshelf::RidleyCompatAPI.module_eval do
 #   alias_method :initialize_original, :initialize
@@ -37,7 +37,7 @@ end
 Berkshelf::RidleyCompatAPI::ClassMethods.module_eval do
   def new_client(**opts)
     client = new(**opts)
-    puts "Created a new client: #{client.inspect}"
+    puts "\n~~~~Created a new client: #{client.inspect}~~~~~~\n"
     if block_given?
       puts "Executing block with the client..."
       yield client
@@ -50,33 +50,41 @@ Berkshelf::RidleyCompatAPI::ClassMethods.module_eval do
   end
 end
 
+Chef::ServerAPI.class_eval do
+  alias_method :original_send_http_request, :send_http_request
 
-
-Chef::HTTP.module_eval do
-
-    def http_client
-      return @http_client if @http_client
-
-      proxy_host = '127.0.0.1'
-      proxy_port = 4443
-
-      @http_client = Net::HTTP.SOCKSProxy(proxy_host, proxy_port).new(@url.host, @url.port, nil)
-      @http_client.use_ssl = @url.scheme == "https"
-      @http_client.verify_mode = OpenSSL::SSL::VERIFY_NONE if @config[:ssl_verify_mode] == :verify_none
-      @http_client.open_timeout = @config[:open_timeout] if @config[:open_timeout]
-      @http_client.read_timeout = @config[:read_timeout] if @config[:read_timeout]
-      @http_client.start
-      ::Kernel.puts "~~~~http_client called~~~~ proxy added ~~~~"
-      @http_client
-    end
-
-    def request(method, url, headers = {}, data = false)
-      ::Kernel.puts "~~~~HTTP request dump~~~~"
-      puts "Chef::HTTP Request (#{method}): #{url}"
-      puts "Headers: #{headers.inspect}"
-      puts "Data: #{data.inspect}" if data
-      # Call the original request method
-      super(method, url, headers, data)
-    end
+  def send_http_request(method, url, headers, data)
+    puts "\n~~~~send_http_request called~~~~\n"
+    original_send_http_request(method, url, headers, data)
+  end
 end
+
+
+# Chef::HTTP.module_eval do
+
+#     def http_client
+#       return @http_client if @http_client
+
+#       proxy_host = '127.0.0.1'
+#       proxy_port = 4443
+
+#       @http_client = Net::HTTP.SOCKSProxy(proxy_host, proxy_port).new(@url.host, @url.port, nil)
+#       @http_client.use_ssl = @url.scheme == "https"
+#       @http_client.verify_mode = OpenSSL::SSL::VERIFY_NONE if @config[:ssl_verify_mode] == :verify_none
+#       @http_client.open_timeout = @config[:open_timeout] if @config[:open_timeout]
+#       @http_client.read_timeout = @config[:read_timeout] if @config[:read_timeout]
+#       @http_client.start
+#       ::Kernel.puts "\n~~~~http_client called~~~~ proxy added ~~~~\n"
+#       @http_client
+#     end
+
+#     def request(method, url, headers = {}, data = false)
+#       ::Kernel.puts "~~~~HTTP request dump~~~~"
+#       puts "Chef::HTTP Request (#{method}): #{url}"
+#       puts "Headers: #{headers.inspect}"
+#       puts "Data: #{data.inspect}" if data
+#       # Call the original request method
+#       super(method, url, headers, data)
+#     end
+# end
 
